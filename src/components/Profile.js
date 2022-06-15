f67d711dbbebe8a59f2a99625d78c041ebd9f406
Nipa <akternipa941@gmail.com>
2022-06-15 16:47:05 +0600

    profile done

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='py-2 flex justify-between items-center bg-gradient-to-b from-[#6202ee] to-[rgba(98,2,238,.5)] text-white'>
+            <h1></h1>
+            <h1 className='text-3xl'>Profile</h1>
+            <h1 className='text-4xl pr-2'> <i class="fa-solid fa-language"></i></h1>
+        </div>
+    );
+};
+
+export default Profile;
